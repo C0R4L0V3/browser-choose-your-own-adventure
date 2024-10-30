d3efcf89e19e4b990a127f4f8d470e08cc001a95
C0R4L0V3 <coraline.e.love@gmail.com>
2024-10-30 14:27:53 -0500

    starting to work on the DOM

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 
 //-- autoplay plays audio media--
-audioElement.play("#gameMusic");    
+// audioElement.play("#gameMusic");    
+
+btns.forEach((btn) => {
+    if (choice1Btn.addEventListener('click', (event) => {
+
+    }))
+    else if (choice2Btn.addEventListener('click', (event) =>{
+
+    }))
+    else if (choice3Btn.addEventListener('click', (event) =>{
+        
+    }))
+
+})
 
 //=============== Constants ===============
 
 //--link files to pull form object arrays--
-const enemies = require('./data');
-const characterRace = require('./data');
-const armors = require('./data');
-const weapons = require('.data');
+// const enemies = require('./data');
+// const characterRace = require('./data');     << improper method
+// const armors = require('./data');
+// const weapons = require('.data');
 
 
-//--query selectors for imgages
-const charImg = document.querySelector("#imgContainerChar")
-const enemyImg = document.querySelector("#imgContainerEnemy")
-const storyImg= document.querySelector("#imgContainerStory")
+//--query selector for Character Stats--
+const userName = document.querySelector("#userName");
+const userHP = document.querySelector("#userHP");
+const userRace = document.querySelector("#userRace");
+const userArm = document.querySelector("#userArm");
+const userWeap = document.querySelector("#userWeap");
+const userAtt = document.querySelector("#userAtt");
+const userDef = document.querySelector("#userDef");
+
+//--query selector for Enemy Stats--
+const enemyName = document.querySelector("#enemyName");
+const enemyAtt = document.querySelector("#enemyAtt");
+const enemyDef = document.querySelector("#enemyDef");
+
+//--query selectors for imgages--
+const charImg = document.querySelector("#imgContainerChar");
+const enemyImg = document.querySelector("#imgContainerEnemy");
+const storyImg= document.querySelector("#imgContainerStory");
 
 //--query selectors for story texts--
-const  reaction = document.querySelector("#storyReaction")
-const  prompt = document.querySelector("#storyPrompt")
+const  reaction = document.querySelector("#storyReaction");
+const  storyPrompt = document.querySelector("#storyPrompt");
 
 //--query selectors for each button & Input--
-const choice1Btn = document.querySelector("#choice1");
-const choice2Btn = document.querySelector("#choice2");
-const choice3Btn = document.querySelector("#choice3");
+const charName = document.querySelector("#charName");
+const btns = document.querySelectorAll("storyChoice")
+const choice1Btn = document.querySelector(".choice1");
+const choice2Btn = document.querySelector(".choice2");
+const choice3Btn = document.querySelector(".choice3");
 
 
 //game object
@@ -37,10 +66,13 @@ const game = {
         attack: 0, 
         defense: 0
     },
-    inventory: [
+    inventory: [        //<<--- possible expansion
         { name: "Potion", quantity: 0,},
         { name: "Arrows", quantity: 0,},
         { name: null, quantity: null,},
+    ],
+    quests :[       //<<-- possible expansion
+        {name: null, completed: false}
     ],
 };
 
@@ -52,3 +84,38 @@ const randomDice = (min, max) => {
     const maxDice = Math.floor(max)
     return Math.floor(Math.random() * (maxDice + minDice) + minDice)
 };
+
+
+const raceDecision = () => {
+    storyPrompt.innerText = "Please Select a Race";
+    choice1Btn.style.visibility = "visible";
+    choice1Btn.innerText = "Human\n(male)";
+    event.target.innerText = "Elf\n(female)";
+    choice3Btn.style.visibility = "visible";
+    choice3Btn.innerText = "Dwarf\n(male)";
+}
+
+
+
+//=============== EventListeners ===============
+
+choice2Btn.addEventListener('click', (event)=> {
+    // console.dir(choice2Btn)
+    // console.log ("clicked")
+    if (event.target.innerText === 'Start Game'){
+        raceDecision()
+    }
+})
+
+btns.forEach((btn) => {
+    if (choice1Btn.addEventListener('click', (event) => {
+
+    }))
+    else if (choice2Btn.addEventListener('click', (event) =>{
+
+    }))
+    else if (choice3Btn.addEventListener('click', (event) =>{
+        
+    }))
+
+})
--- data.js
+++ data.js
@@ -1,26 +1,26 @@
-// ===== game database ======
+// ===== game  OBJECT database ======
 
-const enemies = [
+export const enemies = [
     { name: "bear", hp: 15, attack: 0, defense: 0, loot: []},
     { name: "skeleton", hp: 10, attack: 0, defense: 0, loot: []},
     { name: null, hp: 0, attack: 0, defense: 0, loot: []},
 
 ];
 
-const characterRace =[
+export const characterRace =[
     {name: "Human", attack: 0, defense: 0,},
     {name: "Elf", attack: 0, defense: 0,},
     {name: "Dwarf", attack: 0, defense: 0,},
     // {name: "Orc", attack: 0, defense: 0,},  limiting to three to make thing simplier
 ]
 
-const armors = [
+export const armors = [
     {name: "Leather Armor", defense: 0,},
     {name: "Cloth Robe", defense: 0,},
     {name: "Plate Armor", defense: 0,},
 ];
  
-const weapons = [
+export const weapons = [
     {name: "Sword and Sheild", attack: 0, defense: 0},
     {name: "Bow", attack: 0,},
     {name: "Great Hammer", attack: 0},
@@ -31,6 +31,6 @@ const weapons = [
 //     {name: "arrows"},        //dont think I'll need this object array
 // ]
 
-mainMenu = () => {
-    
-}
+
+// import { name, draw, reportArea, reportPerimeter } from "./modules/square.js";
+// You use the import statement, followed by a comma-separated list of the features you want to import wrapped in curly braces, followed by the keyword from, followed by the module specifier.
