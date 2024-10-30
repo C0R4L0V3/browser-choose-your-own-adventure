c3ea68609344dabaa1648ef1445d7dae20bb38df
C0R4L0V3 <coraline.e.love@gmail.com>
2024-10-30 12:51:44 -0500

    pre-final ccs design

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-//link files to pull form object arrays
 
-audioElement.play("#gameMusic");
+//-- autoplay plays audio media--
+audioElement.play("#gameMusic");    
 
-const enemies = require('./data')
-const characterRace = require('./data')
-const armors = require('./data')
-const weapons = require('.data')
+//=============== Constants ===============
+
+//--link files to pull form object arrays--
+const enemies = require('./data');
+const characterRace = require('./data');
+const armors = require('./data');
+const weapons = require('.data');
+
+
+//--query selectors for imgages
+const charImg = document.querySelector("#imgContainerChar")
+const enemyImg = document.querySelector("#imgContainerEnemy")
+const storyImg= document.querySelector("#imgContainerStory")
+
+//--query selectors for story texts--
+const  reaction = document.querySelector("#storyReaction")
+const  prompt = document.querySelector("#storyPrompt")
+
+//--query selectors for each button & Input--
+const choice1Btn = document.querySelector("#choice1");
+const choice2Btn = document.querySelector("#choice2");
+const choice3Btn = document.querySelector("#choice3");
 
 
 //game object
@@ -19,19 +37,18 @@ const game = {
         attack: 0, 
         defense: 0
     },
-
     inventory: [
         { name: "Potion", quantity: 0,},
         { name: "Arrows", quantity: 0,},
         { name: null, quantity: null,},
-
     ],
+};
 
-}
+//=============== Functions ===============
 
 //Dice generator, can be used for damage or healing
 const randomDice = (min, max) => {
     const minDice = Math.ceil(min)
     const maxDice = Math.floor(max)
     return Math.floor(Math.random() * (maxDice + minDice) + minDice)
-}
+};
